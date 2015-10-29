d47d02afed7c0ce5151b9524ff1f9f29ff735e0d
alumnoFI <alumnoFI@FIW8LC0513.facultades.ort.edu.uy>
2015-10-29 20:24:49 -0300

    ADD BDD

---
+++ src/test/java/ort/isagil/carpaccio/RunCukeTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ort.isagil.carpaccio;
+
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(strict=true, monochrome=true, format="junit:output", snippets=SnippetType.CAMELCASE, features="src/test/resources/ort/isagil/calculator/")
+public class RunCukeTest {
+    
+}
